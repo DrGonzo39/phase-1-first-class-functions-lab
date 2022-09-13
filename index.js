3efbce592eba3332466b24c9e831dd4738784b76
Arthur Benware <artpolitic91@gmail.com>
2022-09-13 18:59:05 -0400

    commit tree 05180c22ae61b1c2d97ba69c5cf1e736930b3bd8

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = (drivers) => { 
+    return drivers.slice(0,2);
+};
+const returnLastTwoDrivers = (drivers) => {
+    return drivers.slice(-2);
+};
+const selectingDrivers = [returnFirstTwoDrivers , returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+    return function(fare) {
+       return fare * multiplier 
+    }
+};
+const fareDoubler = (fare) => {
+    return createFareMultiplier = fare * 2; 
+}
+const fareTripler = (fare) => {
+    return createFareMultiplier = fare * 3;
+}
+const selectDifferentDrivers = (arrayOfDrivers, funk) => {
+    return funk(arrayOfDrivers)
+}
